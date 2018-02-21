6976c3e3bb5b6d390d3a77ad6bb73bf74c27063b
saroj.mishra <saroj.mishra@servpro.in>
2018-02-21 11:22:06 +0530

    Initial Commit

---
+++ webapp/controller/AboutUs.controller.js
@@ -0,0 +1,82 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("tatapower.dev.controller.AboutUs", {
+
+		/**
+		 * Called when a controller is instantiated and its View controls (if available) are already created.
+		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+		 * @memberOf tatapower.dev.view.AboutUs
+		 */
+		//	onInit: function() {
+		//
+		//	},
+	onInit: function(evt) {
+
+			// set explored app's demo model on this sample
+			var oModel = new sap.ui.model.json.JSONModel("json/ContactUs.json");
+			this.getView().setModel(oModel);
+			var vbox1 = this.getView().byId("Flexboxview");
+			var fragment = sap.ui.xmlfragment(["tatapower.dev.fragments.NewConnection.NewConnectionSideMenu"].join("."), this);
+			vbox1.addItem(fragment);
+
+		},
+			handleButtonPress: function(evt) {
+		
+			var vbox = this.getView().byId("FlexboxProcedure");
+			vbox.destroyItems();
+			if (evt.oSource.mProperties.text === "Procedure and Guidelines") {
+				
+				var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.NewConnection.ProcedureAndGuidlines"].join("."), this);
+				vbox.addItem(fragment1);
+			}
+			else if(evt.oSource.mProperties.text === "Application forms") {
+				
+				var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.NewConnection.Applicationforms"].join("."), this);
+				vbox.addItem(fragment1);
+			}
+			else if(evt.oSource.mProperties.text === "FAQs") {
+				
+				var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.NewConnection.FAQs"].join("."), this);
+				vbox.addItem(fragment1);
+			}
+				else if(evt.oSource.mProperties.text === "Apply Online") {
+				
+				var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.NewConnection.ApplyOnline"].join("."), this);
+				vbox.addItem(fragment1);
+			}
+				
+		
+
+		}
+		/**
+		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+		 * (NOT before the first rendering! onInit() is used for that one!).
+		 * @memberOf tatapower.dev.view.AboutUs
+		 */
+		//	onBeforeRendering: function() {
+		//
+		//	},
+
+		/**
+		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+		 * This hook is the same one that SAPUI5 controls get after being rendered.
+		 * @memberOf tatapower.dev.view.AboutUs
+		 */
+		//	onAfterRendering: function() {
+		//
+		//	},
+
+		/**
+		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+		 * @memberOf tatapower.dev.view.AboutUs
+		 */
+		//	onExit: function() {
+		//
+		//	}
+
+	});
+
+});
---
+++ webapp/controller/Home.controller.js
@@ -0,0 +1,137 @@
+sap.ui.define([
+	"tatapower/dev/controller/BaseController"
+], function(BaseController) {
+	"use strict";
+
+	return BaseController.extend("tatapower.dev.controller.Home", {
+
+		onDisplayNotFound: function(oEvent) {
+			// display the "notFound" target without changing the hash
+			this.getRouter().getTargets().display("notFound", {
+				fromTarget: "home"
+			});
+		},
+
+		onInit: function() {
+			this.mobileDropdown();
+			//$("#__panel0").hide();
+
+			/**
+			 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+			 * (NOT before the first rendering! onInit() is used for that one!).
+			 * @memberOf view.MyAccount
+			 */
+		},
+		onBeforeRendering: function() {
+			this.mobileDropdown();
+			//$("#__panel0").hide();
+		},
+
+		/**
+		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+		 * This hook is the same one that SAPUI5 controls get after being rendered.
+		 * @memberOf view.MyAccount
+		 */
+		onAfterRendering: function() {
+			this.mobileDropdown();
+			//$("#__panel0").hide();
+		},
+
+		/**
+		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+		 * @memberOf view.MyAccount
+		 */
+		onExit: function() {
+			this.mobileDropdown();
+			//$("#__panel0").hide();
+		},
+       pressOnlineApplication: function(oEvent) {
+			this.getRouter().navTo("OnlineApplications");
+		},
+		onNavToMyAccount: function(oEvent) {
+			this.getRouter().navTo("MyAccount");
+		},
+		onNavToQuickBill: function(oEvent) {
+			this.getRouter().navTo("PaymentHistory");
+		},
+		onNavToPaybill: function(oEvent) {
+			this.getRouter().navTo("QuickPayment");
+		},
+		onHomePress: function(oEvent) {
+			this.getRouter().navTo("home");
+		},
+
+		OnNewCustomer
+		: function(oEvent) {
+			this.getRouter().navTo("NewCustomer");
+		},
+		pressTariff: function() {
+			this.getRouter().navTo("home");
+		},
+		pressPowerFailure: function() {
+			this.getRouter().navTo("RegisterPowerFailure");
+		},
+		pressPowerOutage: function() {
+			this.getRouter().navTo("home");
+		},
+		pressBeGreen: function() {
+			this.getRouter().navTo("BeGreen");
+		},
+		pressSafeAtHome: function() {
+			this.getRouter().navTo("home");
+		},
+		pressEaseOfBusinessDoing: function() {
+			this.getRouter().navTo("EaseOfDoingBusiness");
+		},
+		pressCustomercare: function() {
+			this.getRouter().navTo("home");
+		},
+		pressBillcalulator: function() {
+			this.getRouter().navTo("Calculator");
+		},
+		pressFAQ: function() {
+			this.getRouter().navTo("FAQ");
+		},
+		pressGrievances: function() {
+			this.getRouter().navTo("Grievances");
+		},
+		pressDigitalServices: function() {
+			this.getRouter().navTo("home");
+		},
+		pressApplicationForms: function() {
+			this.getRouter().navTo("home");
+		},
+		pressPaymentOption: function() {
+			this.getRouter().navTo("home");
+		},
+		pressGreenCorner: function() {
+			this.getRouter().navTo("GreenCorner");
+		},
+		pressKnowledgeCenter: function() {
+			this.getRouter().navTo("KnowledgeCenter");
+		},
+		pressRegulatoryInformation: function() {
+			this.getRouter().navTo("home");
+		},
+		pressTrainedWireman: function() {
+			this.getRouter().navTo("Wireman");
+		},
+		pressCalulator: function() {
+			this.getRouter().navTo("Calculator");
+		},
+		pressVideoGallery: function() {
+			this.getRouter().navTo("home");
+		},
+		pressLatestAdCampaigns: function() {
+			this.getRouter().navTo("LatesAds");
+		},
+		pressNewsletter: function() {
+			this.getRouter().navTo("Newsletter");
+		},
+		pressNewsForyou: function() {
+			this.getRouter().navTo("home");
+		}
+
+	});
+
+});
