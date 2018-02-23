d6ad136ead540e3b75f652b3eff31aa6cab86853
servproSushant <sushant.lawate@servpro.in>
2018-02-23 15:58:33 +0530

    contact us zonal pages completed

--- webapp/controller/ContactUs.controller.js
+++ webapp/controller/ContactUs.controller.js
@@ -13,7 +13,7 @@ sap.ui.define([
 		//	onInit: function() {
 		//
 		//	},
-	onInit: function(evt) {
+		onInit: function(evt) {
 
 			// set explored app's demo model on this sample
 			var oModel = new sap.ui.model.json.JSONModel("json/ContactUs.json");
@@ -21,70 +21,61 @@ sap.ui.define([
 			var vbox1 = this.getView().byId("Flexboxview");
 			var fragment = sap.ui.xmlfragment(["tatapower.dev.fragments.NewConnection.NewConnectionSideMenu"].join("."), this);
 			vbox1.addItem(fragment);
-				var vbox = this.getView().byId("FlexboxProcedure");
-			vbox.destroyItems();
-				var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.ContactUs.AssessmentOfficerVigilance"].join("."), this);
-				vbox.addItem(fragment1);
-
-		},
-			onHomePress: function(oEvent) {
-			this.getRouter().navTo("home");
-		},
-		
-			getRouter: function() {
-			return sap.ui.core.UIComponent.getRouterFor(this);
-		},
-			handleButtonPress: function(evt) {
-		
 			var vbox = this.getView().byId("FlexboxProcedure");
 			vbox.destroyItems();
-			if (evt.oSource.mProperties.text === "Assessment officer vigilance") {
-				
-				var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.ContactUs.AssessmentOfficerVigilance"].join("."), this);
-				vbox.addItem(fragment1);
-			}
-			else if(evt.oSource.mProperties.text === "Call Center Number") {
-				
-				var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.ContactUs.CallCenterNumber"].join("."), this);
-				vbox.addItem(fragment1);
-			}
-			else if(evt.oSource.mProperties.text === "Customer Relations Center") {
-				
-				var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.ContactUs.CustomerRelation"].join("."), this);
-				vbox.addItem(fragment1);
-			}
-				else if(evt.oSource.mProperties.text === "Feedback") {
-				
-				var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.ContactUs.Feedback"].join("."), this);
-				vbox.addItem(fragment1);
-			}
-				else if(evt.oSource.mProperties.text === "HOD Customer Services") {
-				
-				var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.ContactUs.HODCustomerServices"].join("."), this);
-				vbox.addItem(fragment1);
-			}
-				else if(evt.oSource.mProperties.text === "Zonal Managers") {
-				
-				var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.ContactUs.ZonalManagers"].join("."), this);
-				vbox.addItem(fragment1);
-			}
-				else if(evt.oSource.mProperties.text === "Zones and Areas") {
-				
-				var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.ContactUs.ZonesAndAreas"].join("."), this);
-				vbox.addItem(fragment1);
-			}
-				
-		
+			var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.ContactUs.AssessmentOfficerVigilance"].join("."), this);
+			vbox.addItem(fragment1);
 
-		}
-		/**
-		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf tatapower.dev.view.ContactUs
-		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
+		},
+		onHomePress: function(oEvent) {
+			this.getRouter().navTo("home");
+		},
+
+		getRouter: function() {
+			return sap.ui.core.UIComponent.getRouterFor(this);
+		},
+		handleButtonPress: function(evt) {
+
+				var vbox = this.getView().byId("FlexboxProcedure");
+				vbox.destroyItems();
+				if (evt.oSource.mProperties.text === "Assessment officer vigilance") {
+
+					var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.ContactUs.AssessmentOfficerVigilance"].join("."), this);
+					vbox.addItem(fragment1);
+				} else if (evt.oSource.mProperties.text === "Call Center Number") {
+
+					var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.ContactUs.CallCenterNumber"].join("."), this);
+					vbox.addItem(fragment1);
+				} else if (evt.oSource.mProperties.text === "Customer Relations Center") {
+
+					var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.ContactUs.CustomerRelation"].join("."), this);
+					vbox.addItem(fragment1);
+				} else if (evt.oSource.mProperties.text === "Feedback") {
+
+					var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.ContactUs.Feedback"].join("."), this);
+					vbox.addItem(fragment1);
+				} else if (evt.oSource.mProperties.text === "HOD Customer Services") {
+
+					var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.ContactUs.HODCustomerServices"].join("."), this);
+					vbox.addItem(fragment1);
+				} else if (evt.oSource.mProperties.text === "Zonal Managers") {
+					var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.ContactUs.ZonalManagers"].join("."), this);
+					vbox.addItem(fragment1);
+				} else if (evt.oSource.mProperties.text === "Zones and Areas") {
+
+					var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.ContactUs.ZonesAndAreas"].join("."), this);
+					vbox.addItem(fragment1);
+				}
+
+			}
+			/**
+			 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+			 * (NOT before the first rendering! onInit() is used for that one!).
+			 * @memberOf tatapower.dev.view.ContactUs
+			 */
+			//	onBeforeRendering: function() {
+			//
+			//	},
 
 		/**
 		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
