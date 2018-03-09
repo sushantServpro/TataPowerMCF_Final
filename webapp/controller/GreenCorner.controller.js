938cadea3afc512007381eaa4d2de8983d9c297f
servproSushant <sushant.lawate@servpro.in>
2018-03-09 17:00:24 +0530

    Green corner sub pages completed

--- webapp/controller/GreenCorner.controller.js
+++ webapp/controller/GreenCorner.controller.js
@@ -35,48 +35,39 @@ sap.ui.define([
 		},
 		handleButtonPress: function(evt) {
 
-				var vbox = this.getView().byId("FlexboxProcedure");
-				vbox.destroyItems();
-				if (evt.oSource.mProperties.text === "Explore") {
+			var vbox = this.getView().byId("FlexboxProcedure");
+			vbox.destroyItems();
+			if (evt.oSource.mProperties.text === "Explore") {
 
-					var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.GreenCorner.Explore"].join("."), this);
-					vbox.addItem(fragment1);
-				} else if (evt.oSource.mProperties.text === "Conserve") {
+				var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.GreenCorner.Explore"].join("."), this);
+				vbox.addItem(fragment1);
+			} else if (evt.oSource.mProperties.text === "Conserve") {
 
-					var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.GreenCorner.Conserve"].join("."), this);
-					vbox.addItem(fragment1);
-				} else if (evt.oSource.mProperties.text === "Know More") {
+				var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.GreenCorner.Conserve"].join("."), this);
+				vbox.addItem(fragment1);
+			} else if (evt.oSource.mProperties.text === "Green Buildings") {
 
-					var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.BeGreen.AboutDSM"].join("."), this);
-					vbox.addItem(fragment1);
-				}
+				var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.GreenCorner.demoGreenBuild"].join("."), this);
+				vbox.addItem(fragment1);
+			} else if (evt.oSource.mProperties.text === "Combat Climate Change") {
 
+				var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.GreenCorner.demoCombatClimateChange"].join("."), this);
+				vbox.addItem(fragment1);
+			} else if (evt.oSource.mProperties.text === "Energy Efficient Devices") {
+
+				var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.GreenCorner.demoEnergyEfficientDevice"].join("."), this);
+				vbox.addItem(fragment1);
+			} else if (evt.oSource.mProperties.text === "Power Consumption Guidelines") {
+
+				var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.GreenCorner.demoPowerConsumption"].join("."), this);
+				vbox.addItem(fragment1);
+			} else if (evt.oSource.mProperties.text === "Reducing your Bill") {
+
+				var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.GreenCorner.demoReduceYourBill"].join("."), this);
+				vbox.addItem(fragment1);
 			}
-			/**
-			 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-			 * (NOT before the first rendering! onInit() is used for that one!).
-			 * @memberOf tatapower.dev.view.GreenCorner
-			 */
-			//	onBeforeRendering: function() {
-			//
-			//	},
 
-		/**
-		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf tatapower.dev.view.GreenCorner
-		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf tatapower.dev.view.GreenCorner
-		 */
-		//	onExit: function() {
-		//
-		//	}
+		}
 
 	});
 
