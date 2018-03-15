0fa5ad2805cc4e2314003b4c437c154b61a79618
servproSushant <sushant.lawate@servpro.in>
2018-03-15 14:46:01 +0530

    new customer changes

--- webapp/controller/BeGreen.controller.js
+++ webapp/controller/BeGreen.controller.js
@@ -13,7 +13,7 @@ sap.ui.define([
 		//	onInit: function() {
 		//
 		//	},
-		
+
 		onInit: function(evt) {
 
 			// set explored app's demo model on this sample
@@ -22,91 +22,82 @@ sap.ui.define([
 			var vbox1 = this.getView().byId("Flexboxview");
 			var fragment = sap.ui.xmlfragment(["tatapower.dev.fragments.NewConnection.NewConnectionSideMenu"].join("."), this);
 			vbox1.addItem(fragment);
-			
-				var vbox = this.getView().byId("FlexboxProcedure");
-			vbox.destroyItems();
-				var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.BeGreen.AboutDSM"].join("."), this);
-				vbox.addItem(fragment1);
 
-		},
-		
-			handleButtonPress: function(evt) {
-		
 			var vbox = this.getView().byId("FlexboxProcedure");
 			vbox.destroyItems();
-			if (evt.oSource.mProperties.text ==="Introduction") {
-				
+			var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.BeGreen.AboutDSM"].join("."), this);
+			vbox.addItem(fragment1);
+
+		},
+
+		handleButtonPress: function(evt) {
+
+			var vbox = this.getView().byId("FlexboxProcedure");
+			vbox.destroyItems();
+			if (evt.oSource.mProperties.text === "Introduction") {
+
 				var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.BeGreen.Introduction"].join("."), this);
 				vbox.addItem(fragment1);
-			}
-			else if(evt.oSource.mProperties.text === "About DSM") {
-				
+			} else if (evt.oSource.mProperties.text === "About DSM") {
+
 				var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.BeGreen.AboutDSM"].join("."), this);
 				vbox.addItem(fragment1);
-			}
-			else if(evt.oSource.mProperties.text === "DSM Programmes") {
-				
+			} else if (evt.oSource.mProperties.text === "DSM Programmes") {
+
 				var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.BeGreen.DSMProgrammes"].join("."), this);
 				vbox.addItem(fragment1);
-			}
-				else if(evt.oSource.mProperties.text === "Energry Convervation") {
-				
+			} else if (evt.oSource.mProperties.text === "Energy Conservation") {
+
 				var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.BeGreen.EnergyConveration"].join("."), this);
 				vbox.addItem(fragment1);
-			}
-			
-			else if(evt.oSource.mProperties.text === "Contact") {
-				
+			} else if (evt.oSource.mProperties.text === "Contact") {
+
 				var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.BeGreen.Contact"].join("."), this);
 				vbox.addItem(fragment1);
 			}
-			
-			
+
 			// 	else if(evt.oSource.mProperties.text === "Ceiling Fan") {
-				
+
 			// 	var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.BeGreen.ceiling_fan"].join("."), this);
 			// 	vbox.addItem(fragment1);
 			// }
 			// 	else if(evt.oSource.mProperties.text === "Energy Audit") {
-				
+
 			// 	var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.BeGreen.energy_audit"].join("."), this);
 			// 	vbox.addItem(fragment1);
 			// }
 			// 	else if(evt.oSource.mProperties.text ==="Energy Refrigerator") {
-				
+
 			// 	var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.BeGreen.energy_eff_refrigerator"].join("."), this);
 			// 	vbox.addItem(fragment1);
 			// }
 			// 	else if(evt.oSource.mProperties.text === "LED Tube") {
-				
+
 			// 	var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.BeGreen.led_tube"].join("."), this);
 			// 	vbox.addItem(fragment1);
 			// }
 			// 	else if(evt.oSource.mProperties.text === "Split AC") {
-				
+
 			// 	var fragment1 = sap.ui.xmlfragment(["tatapower.dev.fragments.BeGreen.split_ac"].join("."), this);
 			// 	vbox.addItem(fragment1);
 			// }
-				
-				
-		
 
-		}
-,	onHomePress: function(oEvent) {
+		},
+		onHomePress: function(oEvent) {
 			this.getRouter().navTo("home");
 		},
-		
-			getRouter: function() {
-			return sap.ui.core.UIComponent.getRouterFor(this);
-		}
-		/**
-		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf tatapower.dev.view.BeGreen
-		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
+
+		getRouter: function() {
+				return sap.ui.core.UIComponent.getRouterFor(this);
+			}
+			/**
+			 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+			 * (NOT before the first rendering! onInit() is used for that one!).
+			 * @memberOf tatapower.dev.view.BeGreen
+			 */
+			//	onBeforeRendering: function() {
+			//
+			//	},
 
 		/**
 		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
